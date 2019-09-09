883bc19a698b000b60132aeff35238658b80002f
schoeeva000 <schoeeva000@CAM-LAB-26.bgsd.k12.wa.us>
2019-09-09 07:28:19 -0700

    There is an error with the class-idk why

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,9 +1,22 @@
 
-public class Pokemon {
+public class Poke {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println(" (\\__/)");
+		System.out.println(" (o^.^)");
+		System.out.println("z(_(\")(\")");
+		
 
 	}
 
 }
+
+/*
+This is for reference
+
+Pikachu welcomes you to the world of Pokemon!
+ (\__/)
+ (o^.^)
+z(_(“)(“)
+*/ 
